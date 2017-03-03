f8b7742b064c395c5cdae52b14aea801f7660008
captcontrol <pridham.tom@gmail.com>
2017-03-02 22:47:26 -0700

    thx create-react-app bby

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to Plotly Demo</h2>
+                    <h4>It's goin' be ssiiiiiccckkkk</h4>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
